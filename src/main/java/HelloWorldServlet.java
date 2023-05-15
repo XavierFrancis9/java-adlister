21fe59d89f98eb847b3ff1e0e687bb095f93ffb6
Xavier Francis <james.x.francis9@gmail.com>
2023-05-15 15:46:56 -0500

    started servlets exercise

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,14 @@
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+import java.io.PrintWriter;
+@WebServlet
 public class HelloWorldServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse res) {
+        res.setContentType("text/html");
+        PrintWriter out = res.getWriter();
 
+        String name = req.getParameter("name");
     }
 }
