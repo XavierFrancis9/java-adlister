822099fea9d96f0259d25abafcce78a3552c57d3
Xavier Francis <james.x.francis9@gmail.com>
2023-05-19 16:45:43 -0500

    mvc exercise refactored

--- src/main/java/controllers/AdsServlet.java
+++ src/main/java/controllers/AdsServlet.java
@@ -1,5 +1,7 @@
 package controllers;
 
+import dao.Ads;
+import dao.DaoFactory;
 import models.Ad;
 
 import javax.servlet.ServletException;
@@ -16,7 +18,7 @@ public class AdsServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-//    -- gets ListAdsDao
+//    -- gets dao.ListAdsDao
         Ads listAdsDao = DaoFactory.getAdsDao();
 //    -- gets all ads
         List<Ad> ads = listAdsDao.all();
--- src/main/java/controllers/LoginServlet.java
+++ src/main/java/controllers/LoginServlet.java
@@ -1,3 +1,5 @@
+package controllers;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -5,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "LoginServlet", urlPatterns = "/login")
+@WebServlet(name = "controllers.LoginServlet", urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
