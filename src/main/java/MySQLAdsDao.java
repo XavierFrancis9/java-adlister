0f64e50a4620377aeb01fdcea4b8aab4665a2303
Xavier Francis <james.x.francis9@gmail.com>
2023-05-22 16:24:41 -0500

    mysqladsdao constructor complete

---
+++ src/main/java/MySQLAdsDao.java
@@ -0,0 +1,27 @@
+import com.mysql.cj.jdbc.Driver;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.List;
+
+public class MySQLAdsDao implements AdsDao{
+    private Connection connection;
+    public MySQLAdsDao(Config config) {
+        try {
+            DriverManager.registerDriver(new Driver());
+            connection = DriverManager.getConnection(config.getUrl(), config.getUser(), config.getPassword());
+        } catch (SQLException e) {
+            throw new RuntimeException("Error connecting to the database!", e);
+        }
+    }
+    @Override
+    public List<Ad> all() {
+        return null;
+    }
+
+    @Override
+    public Long insert(Ad ad) {
+        return null;
+    }
+}
