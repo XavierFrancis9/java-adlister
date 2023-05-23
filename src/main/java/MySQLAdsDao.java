4e2dff8c434be0bb7980e022f7d19e65c9017871
Xavier Francis <james.x.francis9@gmail.com>
2023-05-22 19:29:42 -0500

    insert method complete

--- src/main/java/MySQLAdsDao.java
+++ src/main/java/MySQLAdsDao.java
@@ -40,9 +40,37 @@ public class MySQLAdsDao implements Ads{
     public Long insert(Ad ad) {
         try {
             Statement stmt = connection.createStatement();
+
+            String insertAd = String.format("INSERT INTO national_parks(name, description, location) VALUES('%s','%s','%s')", ad.getId(), ad.getTitle(), ad.getDescription());
+
+            stmt.executeUpdate(insertAd, Statement.RETURN_GENERATED_KEYS);
+            ResultSet rs = stmt.getGeneratedKeys();
+            rs.next();
+
+            return rs.getLong(1);
             
         } catch (SQLException e) {
             throw new RuntimeException("Error creating statement.");
         }
     }
 }
+
+//   @Override
+//    public Long insert(Ad ad) {
+//        try {
+//            Statement stmt = connection.createStatement();
+//            stmt.executeUpdate(createInsertQuery(ad), Statement.RETURN_GENERATED_KEYS);
+//            ResultSet rs = stmt.getGeneratedKeys();
+//            rs.next();
+//            return rs.getLong(1);
+//        } catch (SQLException e) {
+//            throw new RuntimeException("Error creating a new ad.", e);
+//        }
+//    }
+//
+//    private String createInsertQuery(Ad ad) {
+//        return "INSERT INTO ads(user_id, title, description) VALUES "
+//            + "(" + ad.getUserId() + ", "
+//            + "'" + ad.getTitle() +"', "
+//            + "'" + ad.getDescription() + "')";
+//    }
